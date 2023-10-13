5229cff905aa60a1ba72530f54e2f9b399ce8234
Ayoub Legouirah <ayoubgrand@outlook.fr>
2023-10-13 14:47:15 +0200

    mise a jour

---
+++ variable.js
@@ -0,0 +1,46 @@
+// 1 er exercise
+// let nombre1 = parseInt(prompt("Quel est ton premier chiffre"))
+// let nombre2 = parseInt(prompt("Quel est ton deuxieme chiffre"))
+// let resultat = (nombre1 + nombre2)
+// console.log(resultat)
+
+// 2 eme exercise
+// let nombre1 = parseInt(prompt("Quel est ton premier chiffre"))
+// let nombre2 = parseInt(prompt("Quel est ton deuxieme chiffre"))
+// let resultat = (nombre1 / nombre2)
+// console.log(resultat)
+
+// 3 eme exercise
+// let nombre1 = parseInt(prompt("Quel est ton premier chiffre"))
+// let nombre2 = parseInt(prompt("Quel est ton deuxieme chiffre"))
+// let resultat = (nombre1 % nombre2)
+// console.log(resultat)
+
+// 4 eme exercise
+// let phrase1 = prompt("Quel est ta premiere phrase")
+// let phrase2 = prompt("Quel est ta deuxieme phrase")
+// let resultat = (phrase1 + phrase2)
+// console.log(resultat)
+
+// 5 eme exercise
+// let prix = parseInt(prompt("Prix sans TVA"))
+// let resultat = (prix * 0.21)
+// console.log(resultat + prix)
+
+// 6 eme exercise
+// const PI = 3.14159265359
+// let rayon = parseInt(prompt("rayon"))
+// let resultat = (PI * rayon**2)
+// console.log(resultat)
+
+// 7 eme exercise
+// let heure = parseInt(prompt("Quel est l'heure"))
+// let minute = parseInt(prompt("Quel sont les minutes"))
+// let seconde = parseInt(prompt("Quel sont les secondes"))
+// let resultat = ((heure * 3600) + (minute * 60) + (seconde) )
+// console.log("voila le nombre de seconde depuis minuit " + resultat)
+
+
+
+
+
