7188a26e4dd915275223d0705e0f9aabd8811ee9
Ayoub Legouirah <ayoubgrand@outlook.fr>
2023-10-15 19:12:30 +0200

    mise a jour

---
+++ conditions.js
@@ -0,0 +1,99 @@
+// 1 er exercise:
+// let promo = 8;
+// let tarif = 10;
+// let PeutLePromo = true;
+// let reponse = prompt ("a tu un code promo?")
+
+// if (reponse === 'oui' && PeutLePromo ) {
+//     console.log("Prix à payer:" + promo);
+// } else {
+//     console.log("Prix à payer:" + tarif);
+// }
+
+// 2 eme exercise: 
+// let num1 = prompt ("donne ton premier chiffre")
+// let num2 = prompt ("donne ton deuxieme chiffre")
+// let num3 = prompt ("donne ton troisieme chiffre")
+
+// num1 = parseFloat(num1);
+// num2 = parseFloat(num2);
+// num3 = parseFloat(num3);
+
+// if (num1 > num2, num1 > num3){
+//     console.log ("ton plus grand chiffre est le " + num1 )
+// }
+
+// else if (num2 > num1, num2 > num3){
+//     console.log ("ton plus grand chiffre est le " + num2 )
+// }
+
+// else {
+//     console.log ("ton plus grand chiffre est le " + num3 )
+// }
+
+// 3 eme exercise: 
+
+// const dé1 = Math.floor(Math.random()* 6)
+// console.log("Dés1 = " + dé1)
+// const dé2 = Math.floor(Math.random()* 6)
+// console.log("Dés2 = " + dé2)
+// const dé3 = Math.floor(Math.random()* 6)
+// console.log("Dés3 = " + dé3)
+
+// if (dé1 === dé2 && dé2 === dé3 && dé1 === dé3 ) {
+//     console.log("Trois dés identiques !");
+// } else if (dé1 === dé2 || dé1 === dé3 || dé2 === dé3) {
+//     console.log("Deux dés identiques.");
+// } else {
+//     console.log("Aucun dé identique.");
+// }
+
+
+// 4 eme exercise: 
+// const jour = {
+//     1: "Lundi",
+//     2: "Mardi",
+//     3: "Mercredi",
+//     4: "Jeudi",
+//     5: "Vendredi",
+//     6: "Samedi",
+//     7: "Dimanche"
+// };
+
+// const numero = prompt("Donne un numéro entre 1 et 7");
+// const nomjour = jour[numero];
+
+// if(nomjour){
+//     console.log("Voici ton jour : " + nomjour);
+// }else {
+//     console.log("Tu n'as pas donné le bon numéro");
+// }
+
+// 5 eme exercise:
+// const nombre = Math.floor(Math.random()*40 + 1) 
+// console.log ("Tu as ce nombre à imprimer :" + nombre )
+// let prix
+
+// if (nombre <= 10){
+//     prix = nombre * 0.12
+//     console.log("le prix est " + prix + " euro"  )
+// }
+// else if (nombre > 10 && nombre <= 30){
+//     prix = nombre * 0.11
+//     console.log("le prix est " + prix + " euro" )
+// }
+// else{
+//     prix = nombre * 0.10
+//     console.log("le prix est " + prix + " euro" )    
+// }
+
+
+
+
+
+
+
+
+
+
+
